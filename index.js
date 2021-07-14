c34db8ef659d7e39c3fe0802991cc33ab682d5c7
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-14 04:00:18 -0400

    Add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('ODY0NjM5ODIzNTM3MjQyMTEy.YO4Ylg.b6tiq74yCnKdSPHiypu-Avbnusg');
