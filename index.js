d0fb1bf3ffa0e4770aa24e082e6cd0d5ca05a81a
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-14 04:41:01 -0400

    emoji response try 2

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	let emojis = message.content.match(/<:.+?:\d+>/g)
-	if (typeof emojis != 'undefined') {
+	let emojis = message.content.match(/<:.+?:\d+>/g);
+	if (emojis) {
 		// send back "Pong." to the channel the message was sent in
 		emojis.forEach( e => {
 			message.channel.send('+' + e);
