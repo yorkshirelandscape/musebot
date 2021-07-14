62666ce814ab9fafcaaa967dbbeffac1183d03e5
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-14 04:37:19 -0400

    emoji response

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	console.log(message.content);
+	let emojis = message.content.match(/<:.+?:\d+>/g)
+	if (typeof emojis != 'undefined') {
+		// send back "Pong." to the channel the message was sent in
+		emojis.forEach( e => {
+			message.channel.send('+' + e);
+		})
+	}
 });
 
 client.login(process.env.TOKEN);
