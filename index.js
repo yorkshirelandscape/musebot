a59160e42275be57afa16901288304546f712048
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-14 06:50:49 -0400

    webhook response testing

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-const emojiRegex = require('emoji-regex/RGI_Emoji.js');
+// const emojiRegex = require('emoji-regex/RGI_Emoji.js');
 
 client.on('message', message => {
-	const re = emojiRegex();
-	let match;
-	let emojis = [];
-	while ((match = re.exec(message.content)) != null) {
-		emojis.push(match[0]);
-	  }
+	// const re = emojiRegex();
+	// let match;
+	let emojis = message.guild.emojis;
+	// while ((match = re.exec(message.content)) != null) {
+	// 	emojis.push(match[0]);
+	//   }
 	// emojis = message.content.match(/:.+?:/g);
 	console.log(message);
 	if (emojis) {
