265619bed5d90e0fd9f0d6e47c9e804ec43faa42
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-16 03:58:31 -0400

    test grab from ss

--- index.js
+++ index.js
@@ -66,29 +66,100 @@ function getNewToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
- function listMajors(auth) {
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+const dismoji = require('discord-emoji');
+var result = [];
+
+var emoji1
+var emoji2
+
+function getvalue(auth) {
 	const sheets = google.sheets({version: 'v4', auth});
 	sheets.spreadsheets.values.get({
-	  spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-	  range: 'Class Data!A2:E',
-	}, (err, res) => {
-	  if (err) return console.log('The API returned an error: ' + err);
-	  const rows = res.data.values;
-	  if (rows.length) {
-		console.log('Name, Major:');
-		// Print columns A and E, which correspond to indices 0 and 4.
-		rows.map((row) => {
-		  console.log(`${row[0]}, ${row[4]}`);
-		});
-	  } else {
-		console.log('No data found.');
-	  }
+	spreadsheetId: '1qQBxqku14GTL70o7rpLEQXil1ghXEHff7Qolhu0XrMs',
+	range: 'Dashboard!D4:D6',
+	},(err, res) => {
+	if (err) return console.log('The API returned an error: ' + err);
+	emoji1 = res.data.values[0];
+	emoji2 = res.data.values[1];
 	});
+}
+
+console.log(emoji1 + ' ' + emoji2)
+
+client.on('message', message => {
+
+	if (message.channel.name === 'music-votes') {
+
+	// console.log(message.content);
+
+	const enm = require("emoji-name-map");
+	const one = enm.get('one');
+	const two = enm.get('two');
+
+	let e1 = [];
+	let e2 = [];
+
+	e1 = message.content.match(/:.+?:/g);
+	// console.log(e1);
+	if (e1) { 
+		e1.forEach( e => {
+			e2.push( {name: e.match(/[a-zA-Z0-9_]+/g).toString(), id: null, unicode: null} );
+		})
+	}
+	// console.log(e2);
+	if (e2) {
+		e2.forEach( e => {
+			try {
+				e.id = client.emojis.cache.find(emoji => emoji.name === e.name);
+			} catch (err) {
+				console.log(err);
+			};
+			try {
+				// e.unicode = enm.get(e.name);
+				result = [];
+				getNames(dismoji, e.name);
+				e.unicode = result[0].toString();
+			} catch (err) {
+				console.log(err);
+			}
+		})
+	}
+	// console.log(e2);
+	if (e2) {
+		let i = 0;
+		e2.forEach( e => {
+			if (typeof e.id != 'undefined') {
+				message.react(e.id);
+			} else if (typeof e.unicode != 'undefined' ) {
+				message.react(e.unicode);
+			} else { message.react( i === 0 ? one : two ) };
+			i++;
+		})
+	}
+	}
+});
+
+function getNames(obj, name) {
+	for (var key in obj) {
+	  if (obj.hasOwnProperty(key)) {
+		if ("object" == typeof(obj[key])) {
+		  getNames(obj[key], name);
+		} else if (key == name) {
+		  result.push(obj[key]);
+		}
+	  }
+	}
   }
 
-
+client.login(process.env.TOKEN);
