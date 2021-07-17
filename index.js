d178ccfb61cacc1e178435d774ffcf5826a7e723
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-17 15:19:51 -0400

    test grab from ss

--- index.js
+++ index.js
@@ -78,8 +78,6 @@ client.on('ready', () => {
 		try {
 			let content = fs.readFileSync('credentials.json')
 			var val = await authorize(JSON.parse(content), rng, getMsg);
-			// console.log(val);
-			// channel.send(msg);
 		  } catch (err) {
 			return console.log('Error loading client secret file:', err);
 		  }
@@ -97,7 +95,7 @@ client.on('ready', () => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-async function authorize(credentials, rng, callback) {
+function authorize(credentials, rng, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
@@ -114,7 +112,7 @@ async function authorize(credentials, rng, callback) {
   } catch (err) {
 	return getNewToken(oAuth2Client, callback);
   }
-  return await callback(rng, oAuth2Client);
+  return callback(rng, oAuth2Client);
 }
 
 /**
