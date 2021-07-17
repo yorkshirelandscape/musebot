793cc986afd916ecb67805f01376109a11a3d662
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-17 10:44:12 -0400

    test grab from ss

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function getValue(auth) {
 	  const rows = res.data.values;
 	  if (rows.length) {
 		// Print columns A and E, which correspond to indices 0 and 4.
-		client.channels.cache.fetch('864768873270345788').then(channel => { //751893730117812225
-			channel.send(`${row[0]}, ${row[1]}`);
-		 });
+		let channel = client.channels.cache.get('864768873270345788')
+		//751893730117812225
+		channel.send(`${row[0]}, ${row[1]}`);
 		// rows.map((row) => {
 		// 	console.log(`${row[0]}, ${row[1]}`);
 		// });
