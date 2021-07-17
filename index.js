9a9bc1fe6b5af0a5bc725a2f67c6d1ecc7b3b040
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-17 04:20:13 -0400

    test grab from ss

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -83,17 +83,40 @@ var result = [];
 var emoji1
 var emoji2
 
-function getvalue(auth) {
-	const sheets = google.sheets({version: 'v4', auth});
-	sheets.spreadsheets.values.get({
-	spreadsheetId: '1qQBxqku14GTL70o7rpLEQXil1ghXEHff7Qolhu0XrMs',
-	range: 'Dashboard!D4',
-	},(err, res) => {
-	if (err) return console.log('The API returned an error: ' + err);
-	emoji1 = res.data.values[0];
-	// emoji2 = res.data.values[1];
-	});
-}
+// function getvalue(auth) {
+// 	const sheets = google.sheets({version: 'v4', auth});
+// 	sheets.spreadsheets.values.get({
+// 	spreadsheetId: '1qQBxqku14GTL70o7rpLEQXil1ghXEHff7Qolhu0XrMs',
+// 	range: 'Dashboard!D4',
+// 	},(err, res) => {
+// 	if (err) return console.log('The API returned an error: ' + err);
+// 	emoji1 = res.data.values;
+// 	// emoji2 = res.data.values[1];
+// 	});
+// }
+
+function main () {
+	const authClient = authorize();
+	const request = {
+	  // The ID of the spreadsheet to retrieve data from.
+	  spreadsheetId: '1qQBxqku14GTL70o7rpLEQXil1ghXEHff7Qolhu0XrMs',  // TODO: Update placeholder value.
+  
+	  // The A1 notation of the values to retrieve.
+	  range: 'Dashboard!D4',  // TODO: Update placeholder value.
+  
+	  // How values should be represented in the output.
+	  // The default render option is ValueRenderOption.FORMATTED_VALUE.
+	  valueRenderOption: 'ValueRenderOption.FORMATTED_VALUE',  // TODO: Update placeholder value.
+  
+	  // How dates, times, and durations should be represented in the output.
+	  // This is ignored if value_render_option is
+	  // FORMATTED_VALUE.
+	  // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+	  dateTimeRenderOption: 'ValueRenderOption.FORMATTED_VALUE',  // TODO: Update placeholder value.
+  
+	  auth: authClient,
+	}
+};
 
 console.log(emoji1)
 
