076b7c06ba2c872b7385ac2f5f5f6a6519796192
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-17 11:29:12 -0400

    test grab from ss

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function postMsg(rng) {
 	// Load client secrets from a local file.
 	fs.readFile('credentials.json', (err, content) => {
 		if (err) return console.log('Error loading client secret file:', err);
-		let msg = getValue(rng);
+		let msg = getValue('Dashboard!D3:E6');
 		let channel = client.channels.cache.get('864768873270345788'); //751893730117812225
 		// Authorize a client with credentials, then call the Google Sheets API.
 		authorize(JSON.parse(content), channel.send(msg));
