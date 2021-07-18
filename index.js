c0287a9df338af097832887ac29eabc798b6b9ab
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-07-18 11:46:24 -0400

    trying to edit matchStr

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // time.
 const TOKEN_PATH = 'token.json';
 
+const dismoji = require('discord-emoji');
+const enm = require("emoji-name-map");
+const one = enm.get('one');
+const two = enm.get('two');
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('ready', () => {
+	const guild = client.guilds.cache.get("212660788786102272"); //782213860337647636
 	const channel = client.channels.cache.get('864768873270345788'); //751893730117812225
 
 	let rnd = "Dashboard!B2";
@@ -30,43 +36,135 @@ client.on('ready', () => {
 	let match = 'Dashboard!D3:E6';
 	
 	let now = new Date();
-	let sixam = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 6, 0, 0, 0);
+	let sixam = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 4, 0, 0, 0);
 	let tenpm = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 22, 0, 0, 0);
 
 	getValue(botStat).then(function(bStat) {
 
 		if (now >= sixam && now <= tenpm && bStat === 'GO' ) {
-		
-			getValue(header).then((val) => {
-					if (typeof val != 'undefined') { channel.send(val); }
-				});
-			
 
-			getValue(match).then((val) => {
-					console.log(val);
-					// channel.send(val);
+			let rngArr = [rnd, sng, header, footer, match];
+
+			getValues(rngArr).then((val) => {
+				let rndVal = val[0].values[0].toString();
+				let sngVal = parseInt(val[1].values[0].toString());
+				let headVal = ('values' in val[2] ) ? val[2].values[0].toString() : null;
+				let footVal = ('values' in val[3] ) ? val[3].values[0].toString() : null;
+				
+				if (headVal ? true : false) { channel.send(headVal); }
+
+				let matchArr = [];
+				val[4].values.forEach((row) => {
+					matchArr.push(row[0] + ((typeof row[1] != 'undefined') ? ` ${row[1]}` : '') );
 				});
-			var sngVal;
-			getValue(sng).then((val) => {
-					if (typeof val != 'undefined') { 
-						sngVal = parseInt(val) + 1;
-						setValue(sng, sngVal).then((val) => {
+				matchStr = matchArr.join('\n');
+				let eMatch = matchStr.match(/:.+?:/g);
+				let eCustom = [];
+				if (eMatch) { 
+					eMatch.forEach( eM => {
+						eCustom.push( {text: eM, name: eM.match(/[a-zA-Z0-9_]+/g).toString()} );
+					});
+					if (eCustom) {
+						(function eCust() {
+							return new Promise(resolve => {
+								eCustom.forEach( eC => {
+									// console.log(eC);
+									customEmoji(eC.name).then((eRes) => {
+										// console.log(eRes);
+										try {
+											eC.id = eRes.id;
+										} catch (err) { 
+											console.log(err);
+										}
+									});
+								});
+							resolve(eCustom);
+							});	
+						})().then(function(eC1) {
+							return new Promise(resolve => {
+								let eC2 = eC1.filter( function(eCSet){
+									return eCSet.hasOwnProperty('id');
+								})
+								resolve(eC2);
+							});
+						}).then(function(eC3) {
+							return new Promise(resolve => {
+								console.log(eC3);
+								eC3.forEach( eC4 => {
+									matchStr.replace(eC4.text, `<${eC4.text}${eC4.id}>`);
+								});
+								resolve(matchStr);
+							});
+						}).then((mStr) => {
+							console.log(mStr);
+							if (mStr ? true : false ) {
+								channel.send(mStr).then((sent) => {
+									let emo = emote(mStr);
+									if (emo) {
+										let i = 0;
+										emo.forEach( e => {
+											if (typeof e.id != 'undefined') {
+												sent.react(e.id);
+											} else if (typeof e.unicode != 'undefined' ) {
+												sent.react(e.unicode);
+											} else { sent.react( i === 0 ? one : two ) };
+											i++;
+										})
+									}
+								});
+								console.log(sngVal);					
+								sngVal = sngVal + 1;
+								setValue(sng, sngVal).then((val) => {
+									console.log(val);
+								});
+							}
+						});
+					}
+				}
+
+				if (footVal ? true : false) { 
+					channel.send(footVal).then((sent) => {
+						let emo = emote(footVal);
+						if (emo) {
+							let i = 0;
+							emo.forEach( e => {
+								if (typeof e.id != 'undefined') {
+									sent.react(e.id);
+								} else if (typeof e.unicode != 'undefined' ) {
+									sent.react(e.unicode);
+								} else { sent.react( i === 0 ? one : two ) };
+								i++;
+							})
+						}
+					});
+					if (typeof rndVal != 'undefined') { 
+						rndVal = 'R' + (parseInt(rndVal.slice(1,2)) + 1);
+						setValue(rnd, rndVal).then((val) => {
 							console.log(val);
 						});
-					 }
-				});
-
-				
-			getValue(footer).then((val) => {
-				if (typeof val != 'undefined') { 
-					channel.send(val); 
-					// rnd.setValue('R' + rnd.getValue().slice(1) + 1);
-					// sng.setValue(1);
-					// botStat.setValue('STOP');
+					}
+					setValue(sng, 1).then((val) => {
+						console.log(val);
+					});
+					setValue(botStat, 'STOP').then((val) => {
+						console.log(val);
+					});
 				}
 			});
 		}
 	});
+	
+	function customEmoji(emoName) {
+		return new Promise(resolve => {
+			try {
+				// console.log(guild.emojis.cache.find(ident => ident.name === emoName));
+				resolve( guild.emojis.cache.find(ident => ident.name === emoName) );
+			} catch (err) {
+				console.log('Not a custom emoji.');
+				resolve(null);
+			};			
+		});
+	}
 });
 
 
@@ -79,6 +177,19 @@ function getValue(rng) {
 			resolve(console.log('Error loading client secret file:', err));
 		}
 		resolve( authorize(content).then((auth) => getMsg(rng, auth)) );
+	});
+}
+
+
+function getValues(rng) {
+	return new Promise(resolve => {
+		// Load client secrets from a local file.
+		try {
+			var content = JSON.parse(fs.readFileSync('credentials.json'))
+		} catch (err) {
+			resolve(console.log('Error loading client secret file:', err));
+		}
+		resolve( authorize(content).then((auth) => getMsgs(rng, auth)) );
 	});
 }
 
@@ -164,6 +275,20 @@ async function getMsg(rng, auth) {
   return msg;
 }
 
+async function getMsgs(rng, auth) {
+	const sheets = google.sheets({version: 'v4', auth});
+	try {
+	  var response = await sheets.spreadsheets.values.batchGet({
+		spreadsheetId: '1qQBxqku14GTL70o7rpLEQXil1ghXEHff7Qolhu0XrMs',
+		ranges: rng,
+	  })
+	} catch (err) {
+	  console.log('The API returned an error: ' + err);
+	  throw(err);
+	}
+	return response.data.valueRanges;
+  }
+
 
 async function setMsg(rng, val, auth) {
 	const sheets = google.sheets({version: 'v4', auth});
@@ -182,6 +307,64 @@ async function setMsg(rng, val, auth) {
 	  throw(err);
 	}
 	return confirm.config.data.values[0];
-  }
+}
+
+
+
+var result = [];
+
+var emoji1
+var emoji2
+
+function emote(e) {
+	let e1 = [];
+	let e2 = [];
+
+	e1 = e.match(/:.+?:/g);
+	// console.log(e1);
+	if (e1) { 
+		e1.forEach( e => {
+			e2.push( {name: e.match(/[a-zA-Z0-9_]+/g).toString(), id: null, unicode: null} );
+		})
+	}
+
+	if (e2) {
+		e2.forEach( e => {
+			try {
+				e.id = client.emojis.cache.find(emoji => emoji.name === e.name);
+			} catch (err) {
+				console.log(err);
+			};
+			try {
+				result = [];
+				getNames(dismoji, e.name);
+				e.unicode = result[0].toString();
+			} catch (err) {
+				console.log('Custom or missing emoji');
+			}
+		})
+	}
+
+	let eArr = [];
+	if (e2) {
+		e2.forEach( e => {
+			eArr.push(e);
+		});
+	}
+	return eArr;
+}
+
+function getNames(obj, name) {
+	for (var key in obj) {
+	  if (obj.hasOwnProperty(key)) {
+		if ("object" == typeof(obj[key])) {
+		  getNames(obj[key], name);
+		} else if (key == name) {
+		  result.push(obj[key]);
+		}
+	  }
+	}
+}
+
 
 client.login(process.env.TOKEN);
