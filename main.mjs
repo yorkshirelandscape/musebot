01d2d30e8794590a77e0d83d51f0dff544c57509
Randy <rpfolse@gmail.com>
2021-08-10 23:36:30 -0500

    Add a new modularized bot
    Lots of updates here
    * Clean up dependencies
    * Add pino for logging (and pino-pretty for dev/console logging)
    * Add .nvmrc to specify node version
    * Multiple .env files, per environment
      TODO Fix this somehow, right now it's unwieldy and requires special handling
        for secret things
    * Add a whole slew of new files for running the bot via individual commands
      These are all set up as ES6 module files (mjs)
      * `main` as an entry point to the app, handles options and creates the bot
      * `muse-bot` as the actual bot which defines specific actions and delegates
        to individual modules
      * `muse-google` to handle authenticating and interacting with the Google
        Sheets API
      * `muse-discord` to handle interacting with the Discord.js client
    
    This replicates a tiny bit of functionality from (an old version of) test.js,
    but mostly just enough to prove that it works and is able to talk to the
    services we need.
    
    Big TODOs left around cleaning up logging, esp for dev vs prod environments,
    and between separating config and behavior between those environments,
    including leaving space for actually secret stuff.

---
+++ main.mjs
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+import { fileURLToPath } from 'url';
+
+import dotenv from 'dotenv';
+import pino from 'pino';
+import Getopt from 'node-getopt';
+
+import MuseBot from './muse-bot.mjs';
+
+/**
+ * Configures Getopt and returns parsed command line arguments
+ * @returns Initialized and parsed options object
+ */
+const getOptions = () => new Getopt([
+  ['t', 'testing', 'Use test spreadsheet and Discord server'],
+  ['n', 'dry-run', "Don't post to Discord or modify any spreadsheets"],
+  ['f', 'force', 'Skip checking if the bot should be active and force action'],
+  ['h', 'help', 'Display this help message'],
+  ['v', 'verbose', 'Print more debug messages'],
+  ['', 'json', 'Format log messages as JSON objects'],
+]).bindHelp().parseSystem();
+
+/**
+ * Gets the log level based on the LOG_LEVEL environment variable and the
+ * `verbose` argument.
+ *
+ * If set, the environment variable will override the verbose flag.
+ *
+ * @param {boolean} verbose - Flag indicating whether to default to setting
+ *   the log level to `info` or the more verbose `debug`
+ */
+const getLogLevel = (verbose) => process.env.LOG_LEVEL || (verbose ? 'debug' : 'info');
+
+/**
+ * Creates a new pino logger instance and initializes it to the given log level.
+ *
+ * @param {string} level - The log level to configure the new logger with
+ */
+const getLogger = (level, pretty) => pino({ level, prettyPrint: pretty });
+
+const main = async () => {
+  const opt = getOptions();
+  dotenv.config({ path: opt.options.testing ? 'dev.env' : 'prod.env' });
+  const logger = getLogger(getLogLevel(opt.options.verbose), !opt.options.json);
+  const bot = new MuseBot(logger, opt.options);
+  await bot.init();
+  await bot.process(opt.argv || []);
+  await bot.teardown();
+};
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  // Script is invoked directly rather than imported
+  main();
+}
---
+++ muse-bot.mjs
@@ -0,0 +1,156 @@
+import MuseDiscord from './discord/muse-discord.mjs';
+import MuseGoogle from './google/muse-google.mjs';
+
+const START_TIME = 5;
+const END_TIME = 21;
+
+/**
+ * MuseBot class for initializing and running the Discord bot.
+ */
+export default class MuseBot {
+  constructor(logger, options) {
+    this.logger = logger.child({ class: 'MuseBot' });
+    this.options = options;
+    this.startTime = new Date();
+    this.endTime = null;
+    this.initialized = false;
+    this.discordClient = null;
+    this.destroyClient = true;
+  }
+
+  /**
+   * Initializes the bot. This includes the following:
+   * - Initializes the Discord client
+   */
+  async init() {
+    this.logger.info('Initializing Discord client');
+    this.discordClient = new MuseDiscord(this.logger);
+    this.googleClient = new MuseGoogle(this.logger);
+    await Promise.all([
+      this.discordClient.init(),
+      this.googleClient.init(),
+    ]);
+    this.initialized = true;
+  }
+
+  /**
+   * Internal function to check that the bot has been initialized.
+   *
+   * If the bot is not initialized when this is invoked, it throws an error.
+   */
+  requireInit() {
+    if (!this.initialized) {
+      throw new Error('Bot was not initialized');
+    }
+  }
+
+  /**
+   * Async function that says whether the bot should be enabled.
+   *
+   * If the `force` option is set, this will always return `true`. Otherwise,
+   * will only return `true` if the bot's state is set to `GO` in the
+   * spreadsheet and the current time is in the bot's allowed operating window.
+   */
+  async isBotEnabled() {
+    const botState = await this.googleClient.getBotState();
+    const nowHour = new Date().getHours();
+    return botState === 'GO' && START_TIME < nowHour && nowHour < END_TIME;
+  }
+
+  /**
+   * Iterates through the command line arguments to perform the requested
+   * actions.
+   *
+   * Requires the bot to have been initialized first.
+   *
+   * Though async, this function waits for each action to fully complete before
+   * moving on to the next action.
+   *
+   * See handleAction method for information about allowed actions.
+   *
+   * @param {string[]} actions - Array of actions to perform
+   */
+  async process(actions) {
+    this.requireInit();
+    for (const action of actions) {
+      // Use for...of so we can await each action in sequence
+      // eslint-disable-next-line no-await-in-loop
+      await this.handleAction(action);
+    }
+  }
+
+  /**
+   * Dispatcher function to handle individual actions.
+   *
+   * Possible actions are:
+   * - `match` Posts a single match
+   * - `start` Starts the daemon to post matches, count reactions, etc.
+   * - `status` Prints the status of the current round
+   *
+   * This function is synchronous and waits for the action to fully complete
+   * before returning.
+   *
+   * @param {string} action - The name of the action to perform, as described
+   *   above.
+   */
+  async handleAction(action) {
+    this.requireInit();
+    this.logger.debug(`Processing argument "${action}"`);
+    switch (action) {
+      case 'match':
+        // Post one match
+        this.logger.info('Posting one match');
+        // this.client.postMatch(options);
+        break;
+      case 'start':
+        // Do all the bot things, keep running:
+        // * Start round
+        // * Post matches
+        // * Collect reactions
+        // * End round when appropriate
+        this.logger.info('Starting daemon');
+        // this.client.start(options)
+        this.destroyClient = false;
+        break;
+      case 'status':
+        // Print out status of current bracket/round
+        this.logger.info('Status of current round');
+        break;
+      case 'testGoogle':
+        // We authorize the client on init, this is to verify that worked
+        this.logger.info('Test Google API authorization');
+        await this.googleClient.testAuthorization();
+        break;
+      case 'testDiscord':
+        // We authorize the client on init, this is to verify that worked
+        this.logger.info('Test Discord API authorization');
+        await this.discordClient.testAuthorization();
+        break;
+      case 'testEmoji':
+        this.logger.info('Test finding emoji');
+        await this.discordClient.findRecentEmoji();
+        break;
+      default:
+        this.logger.error(`Unknown action: "${action}"`);
+    }
+  }
+
+  /**
+   * This function tears down the bot.
+   *
+   * This includes the following actions:
+   * - Logging the elapsed time
+   * - Destroying the Discord client (if applicable)
+   */
+  teardown() {
+    this.requireInit();
+    this.endTime = new Date();
+    const elapsed = ((this.endTime - this.startTime) / 1000).toFixed(3);
+    this.logger.info(`Completed processing arguments. Elapsed time ${elapsed}s`);
+    if (this.destroyClient) {
+      this.logger.info('Destroying Discord client');
+      this.discordClient.destroy();
+    }
+    this.initialized = false;
+  }
+}
