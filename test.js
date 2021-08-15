a2f3dd4655b771d1bd3580f4bf93b0015ff8a8d6
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-08-15 07:31:26 -0400

    test.js: condition for checkRounds

--- test.js
+++ test.js
@@ -110,199 +110,203 @@ const compare = (a, b) => {
 
 // function that pulls it all together
 const checkRound = async () => {
-  const channel = client.channels.cache.get(CHANNEL_ID);
-  const musicChan = client.channels.cache.get(MUSIC_ID);
-  const testChan = client.channels.cache.get(TEST_VOTES);
-  const testMusic = client.channels.cache.get(SKYNET);
+  const botState = await getValue(BOT_STATE_REF);
 
-  // Even though REFS is an object, order is guaranteed for non-string keys
-  const valueRanges = await getValues(Object.values(REFS));
+  if (botState === 'STOP') {
+    const channel = client.channels.cache.get(CHANNEL_ID);
+    const musicChan = client.channels.cache.get(MUSIC_ID);
+    const testChan = client.channels.cache.get(TEST_VOTES);
+    const testMusic = client.channels.cache.get(SKYNET);
 
-  const round = valueRanges[0].values[0].toString();
-  const size = parseInt(valueRanges[5].values[0].toString());
-  // const year = parseInt(valueRanges[6].values[0].toString());
-  const rndVal = parseInt(round.slice(1));
+    // Even though REFS is an object, order is guaranteed for non-string keys
+    const valueRanges = await getValues(Object.values(REFS));
 
-  // fetch the last 150 messages (this should cover even the longest rounds)
-  fetchMany(channel, 150).then(async (messages) => {
-    // of those, find the most recent messages that begin and end a round
-    const roundStart = await messages.find((msg) => msg.content.includes('Begins————'));
-    const roundEnd = await messages.find((msg) => msg.content.includes('you have checked in and are done voting'));
+    const round = valueRanges[0].values[0].toString();
+    const size = parseInt(valueRanges[5].values[0].toString());
+    // const year = parseInt(valueRanges[6].values[0].toString());
+    const rndVal = parseInt(round.slice(1));
 
-    // if the most recent round is complete,
-    // fetch the reactions from the check-in and check-out messages
-    if (roundStart.createdTimestamp < roundEnd.createdTimestamp) {
-      const checkIns = await getChecks(channel, 'if you plan on voting');
-      const checkOuts = await getChecks(channel, 'you have checked in and are done voting');
+    // fetch the last 150 messages (this should cover even the longest rounds)
+    fetchMany(channel, 150).then(async (messages) => {
+      // of those, find the most recent messages that begin and end a round
+      const roundStart = await messages.find((msg) => msg.content.includes('Begins————'));
+      const roundEnd = await messages.find((msg) => msg.content.includes('you have checked in and are done voting'));
 
-      // find the check-ins without check-outs and vice versa, then calculate the pct checked in
-      const missing = checkIns.filter((x) => !checkOuts.map((u) => u.user).includes(x.user));
-      const missingList = missing.map((u) => u.user).join(', ');
-      const missingTagList = missing.map((u) => `<@!${u.id}>`).join(', ');
-      const extra = checkOuts.filter((x) => !checkIns.map((u) => u.user).includes(x.user));
-      const extraList = extra.map((u) => u.user).join(', ');
-      const extraTagList = extra.map((u) => `<@!${u.id}>`).join(', ');
-      const pctCheckedIn = (checkOuts.length - extra.length) / checkIns.length;
+      // if the most recent round is complete,
+      // fetch the reactions from the check-in and check-out messages
+      if (roundStart.createdTimestamp < roundEnd.createdTimestamp) {
+        const checkIns = await getChecks(channel, 'if you plan on voting');
+        const checkOuts = await getChecks(channel, 'you have checked in and are done voting');
 
-      const roundEndTime = DateTime.fromMillis(roundEnd.createdTimestamp);
+        // find the check-ins without check-outs and vice versa, then calculate the pct checked in
+        const missing = checkIns.filter((x) => !checkOuts.map((u) => u.user).includes(x.user));
+        const missingList = missing.map((u) => u.user).join(', ');
+        const missingTagList = missing.map((u) => `<@!${u.id}>`).join(', ');
+        const extra = checkOuts.filter((x) => !checkIns.map((u) => u.user).includes(x.user));
+        const extraList = extra.map((u) => u.user).join(', ');
+        const extraTagList = extra.map((u) => `<@!${u.id}>`).join(', ');
+        const pctCheckedIn = (checkOuts.length - extra.length) / checkIns.length;
 
-      // calculate round limits
-      let roundMin = 11;
-      let roundMax = 23;
-      if (rndVal === 0
-        || (rndVal === 1 && size === 96)
-        || (rndVal === 2 && (size === 64 || size === 48))
-        || (rndVal === 3 && size < 64)) {
-        roundMin = 23;
-        roundMax = 35;
-      } else if (round === '3P') {
-        roundMin = 23;
-        roundMax = 23;
-      }
+        const roundEndTime = DateTime.fromMillis(roundEnd.createdTimestamp);
 
-      if (roundEndTime.plus({ hours: roundMax }) > 20
-        || roundEndTime.plus({ hours: roundMax }) < 5) {
+        // calculate round limits
+        let roundMin = 11;
+        let roundMax = 23;
+        if (rndVal === 0
+          || (rndVal === 1 && size === 96)
+          || (rndVal === 2 && (size === 64 || size === 48))
+          || (rndVal === 3 && size < 64)) {
+          roundMin = 23;
+          roundMax = 35;
+        } else if (round === '3P') {
+          roundMin = 23;
+          roundMax = 23;
+        }
+
+        if (roundEndTime.plus({ hours: roundMax }) > 20
+          || roundEndTime.plus({ hours: roundMax }) < 5) {
+          now = DateTime.now();
+          const tmrwStart = DateTime.now();
+          tmrwStart.plus({ days: 1 });
+          tmrwStart.set({ hour: 5 });
+          tmrwStart.set({ minute: 0 });
+          tmrwStart.set({ millisecond: 0 });
+          roundMax += tmrwStart.diff(roundEndTime, 'hours');
+        }
+
+        // if 80% are checked in and the round is half over OR
+        // the round has one hour left to go, issue the 1-hour warning
         now = DateTime.now();
-        const tmrwStart = DateTime.now();
-        tmrwStart.plus({ days: 1 });
-        tmrwStart.set({ hour: 5 });
-        tmrwStart.set({ minute: 0 });
-        tmrwStart.set({ millisecond: 0 });
-        roundMax += tmrwStart.diff(roundEndTime, 'hours');
-      }
+        if ((pctCheckedIn >= 0.8 && now > roundEndTime.plus({ hours: roundMin }))
+            || now > roundEndTime.plus({ hours: roundMax })) {
+          if (pctCheckedIn < 1) {
+            const msg = `One-Hour Warning
+              ${(pctCheckedIn * 100).toFixed(1)}% checked in.
+              Missing: ${missingTagList}
+              ${extraTagList ? `\nExtra: ${extraTagList}` : ''}`;
+            await musicChan.send(msg);
+            await testMusic.send(msg);
 
-      // if 80% are checked in and the round is half over OR
-      // the round has one hour left to go, issue the 1-hour warning
-      now = DateTime.now();
-      if ((pctCheckedIn >= 0.8 && now > roundEndTime.plus({ hours: roundMin }))
-          || now > roundEndTime.plus({ hours: roundMax })) {
-        if (pctCheckedIn < 1) {
-          const msg = `One-Hour Warning
-            ${(pctCheckedIn * 100).toFixed(1)}% checked in.
-            Missing: ${missingTagList}
-            ${extraTagList ? `\nExtra: ${extraTagList}` : ''}`;
-          await musicChan.send(msg);
-          await testMusic.send(msg);
-
-          // wait an hour for the round to end, then tabulate the results
-          await sleep(60 * 60 * 1000);
-        }
-
-        await musicChan.send('Round concluded. Tabulating votes.');
-        await testMusic.send('Round concluded. Tabulating votes.');
-
-        // fetch 100 most recent messages (not necessary, but I wrote this out of order)
-        const roundMessages = await channel.messages.fetch({ limit: 100 });
-
-        // isolate the check-out messages and convert to an array
-        const msgDelims = roundMessages.filter((msg) => msg.content.includes('you have checked in and are done voting') && msg.deleted === false);
-        // Array.from(msgDelims);
-        // console.log(msgDelims);
-        // filter all the messages for those between the two most recent delimiters
-        const rndMatches = roundMessages.filter((msg) => (
-          msg.createdTimestamp < msgDelims.first(2)[0].createdTimestamp
-          && msg.createdTimestamp > msgDelims.first(2)[1].createdTimestamp
-          && msg.deleted === false && msg.content.includes('Match')
-        ));
-        // create an array of the reaction counts for each message
-        const rndMatchesResults = [];
-        rndMatches.forEach((rm) => {
-          const matchNo = parseInt(rm.content.slice(8, rm.content.indexOf(':')));
-          const matchReacts = [];
-          rm.reactions.cache.forEach((r) => {
-            const emoKey = r.emoji.id ? `<:${r.emoji.name}:${r.emoji.id}>` : r.emoji.name;
-            matchReacts.push({ [emoKey]: r.count });
-          });
-          rndMatchesResults.push({ [matchNo]: matchReacts });
-        });
-
-        // format the results array properly
-        const resultsArray = [];
-        rndMatchesResults.forEach((m) => {
-          Object.values(m).forEach((c) => {
-            const tieRand = Math.round(Math.random());
-            const tieEmoji = Object.keys(c[tieRand]).toString();
-            const tieC1 = parseInt(Object.values(c[0]).toString());
-            const tieC2 = parseInt(Object.values(c[1]).toString());
-            const tie = tieC1 === tieC2 ? 1 : 0;
-            resultsArray.push({
-              c1: tie === 1 && tieRand === 0 ? tieC1 + 1 : tieC1,
-              c2: tie === 1 && tieRand === 1 ? tieC2 + 1 : tieC2,
-              tie,
-              match: Object.keys(m)[0],
-              winner: tie === 1 ? tieRand + 1 : 0,
-              emoji: tie === 1 ? tieEmoji : null,
-            });
-          });
-        });
-        // resultsArray.reverse();
-        resultsArray.sort(compare);
-
-        const pushArray = [];
-        resultsArray.forEach((r) => {
-          pushArray.push([r.c1, r.c2, r.tie]);
-        });
-
-        // set the range to push the results to and push them
-        const lastRound = parseInt(round.slice(1)) - 1;
-        const resultsRange = `R${lastRound}!K2:M${(2 ** (7 - rndVal)) + 1}`;
-        setValues(resultsRange, pushArray);
-
-        await setValue(BOT_STATE_REF, 'GO');
-
-        // announce tie results
-        const tiesArray = resultsArray.filter((m) => m.tie === 1);
-
-        if (tiesArray.length > 0) {
-          await musicChan.send('Settling ties.');
-          await testChan.send('Settling ties.');
-          tiesArray.forEach(async (t) => {
-            const message = await rndMatches.find((msg) => parseInt(msg.content.slice(8, msg.content.indexOf(':'))) === parseInt(t.match));
-            await musicChan.send(message.content.replace('**Match', '**Tie'));
-            await testMusic.send(message.content.replace('**Match', '**Tie'));
-            await sleep(5 * 1000);
-            await musicChan.send(`Winner: ${t.emoji}`);
-            await testMusic.send(`Winner: ${t.emoji}`);
-            await sleep(3 * 1000);
-          });
-        }
-
-        if (round !== '3P') {
-          let msg = '';
-          if (roundEndTime.plus({ hours: roundMax }) > 20
-            || roundEndTime.plus({ hours: roundMax }) < 5) {
-            now = DateTime.now();
-            const tomorrow = now.plus({ days: 1 });
-            const fiveamtomorrow = DateTime.fromObject({
-              year: tomorrow.year,
-              month: tomorrow.month,
-              day: tomorrow.day,
-              hour: 5,
-            });
-            msg = `The next round will begin at <t:${fiveamtomorrow.valueOf() / 1000}:F>`;
-          } else {
-            now = DateTime.now();
-            const resumeTime = Duration.fromObject({
-              hours: 1 - (now.hour % 2),
-              minutes: 60 - now.minute,
-            }).toFormat('h m');
-            msg = `The next round will begin in ${resumeTime.hours > 0 ? `${resumeTime.hours}h` : ''}${resumeTime.minutes}m.`;
+            // wait an hour for the round to end, then tabulate the results
+            await sleep(60 * 60 * 1000);
           }
-          await musicChan.send(msg);
-          await testChan.send(msg);
+
+          await musicChan.send('Round concluded. Tabulating votes.');
+          await testMusic.send('Round concluded. Tabulating votes.');
+
+          // fetch 100 most recent messages (not necessary, but I wrote this out of order)
+          const roundMessages = await channel.messages.fetch({ limit: 100 });
+
+          // isolate the check-out messages and convert to an array
+          const msgDelims = roundMessages.filter((msg) => msg.content.includes('you have checked in and are done voting') && msg.deleted === false);
+          // Array.from(msgDelims);
+          // console.log(msgDelims);
+          // filter all the messages for those between the two most recent delimiters
+          const rndMatches = roundMessages.filter((msg) => (
+            msg.createdTimestamp < msgDelims.first(2)[0].createdTimestamp
+            && msg.createdTimestamp > msgDelims.first(2)[1].createdTimestamp
+            && msg.deleted === false && msg.content.includes('Match')
+          ));
+          // create an array of the reaction counts for each message
+          const rndMatchesResults = [];
+          rndMatches.forEach((rm) => {
+            const matchNo = parseInt(rm.content.slice(8, rm.content.indexOf(':')));
+            const matchReacts = [];
+            rm.reactions.cache.forEach((r) => {
+              const emoKey = r.emoji.id ? `<:${r.emoji.name}:${r.emoji.id}>` : r.emoji.name;
+              matchReacts.push({ [emoKey]: r.count });
+            });
+            rndMatchesResults.push({ [matchNo]: matchReacts });
+          });
+
+          // format the results array properly
+          const resultsArray = [];
+          rndMatchesResults.forEach((m) => {
+            Object.values(m).forEach((c) => {
+              const tieRand = Math.round(Math.random());
+              const tieEmoji = Object.keys(c[tieRand]).toString();
+              const tieC1 = parseInt(Object.values(c[0]).toString());
+              const tieC2 = parseInt(Object.values(c[1]).toString());
+              const tie = tieC1 === tieC2 ? 1 : 0;
+              resultsArray.push({
+                c1: tie === 1 && tieRand === 0 ? tieC1 + 1 : tieC1,
+                c2: tie === 1 && tieRand === 1 ? tieC2 + 1 : tieC2,
+                tie,
+                match: Object.keys(m)[0],
+                winner: tie === 1 ? tieRand + 1 : 0,
+                emoji: tie === 1 ? tieEmoji : null,
+              });
+            });
+          });
+          // resultsArray.reverse();
+          resultsArray.sort(compare);
+
+          const pushArray = [];
+          resultsArray.forEach((r) => {
+            pushArray.push([r.c1, r.c2, r.tie]);
+          });
+
+          // set the range to push the results to and push them
+          const lastRound = parseInt(round.slice(1)) - 1;
+          const resultsRange = `R${lastRound}!K2:M${(2 ** (7 - rndVal)) + 1}`;
+          setValues(resultsRange, pushArray);
+
+          await setValue(BOT_STATE_REF, 'GO');
+
+          // announce tie results
+          const tiesArray = resultsArray.filter((m) => m.tie === 1);
+
+          if (tiesArray.length > 0) {
+            await musicChan.send('Settling ties.');
+            await testChan.send('Settling ties.');
+            tiesArray.forEach(async (t) => {
+              const message = await rndMatches.find((msg) => parseInt(msg.content.slice(8, msg.content.indexOf(':'))) === parseInt(t.match));
+              await musicChan.send(message.content.replace('**Match', '**Tie'));
+              await testMusic.send(message.content.replace('**Match', '**Tie'));
+              await sleep(5 * 1000);
+              await musicChan.send(`Winner: ${t.emoji}`);
+              await testMusic.send(`Winner: ${t.emoji}`);
+              await sleep(3 * 1000);
+            });
+          }
+
+          if (round !== '3P') {
+            let msg = '';
+            if (roundEndTime.plus({ hours: roundMax }) > 20
+              || roundEndTime.plus({ hours: roundMax }) < 5) {
+              now = DateTime.now();
+              const tomorrow = now.plus({ days: 1 });
+              const fiveamtomorrow = DateTime.fromObject({
+                year: tomorrow.year,
+                month: tomorrow.month,
+                day: tomorrow.day,
+                hour: 5,
+              });
+              msg = `The next round will begin at <t:${fiveamtomorrow.valueOf() / 1000}:F>`;
+            } else {
+              now = DateTime.now();
+              const resumeTime = Duration.fromObject({
+                hours: 1 - (now.hour % 2),
+                minutes: 60 - now.minute,
+              }).toFormat('h m');
+              msg = `The next round will begin in ${resumeTime.hours > 0 ? `${resumeTime.hours}h` : ''}${resumeTime.minutes}m.`;
+            }
+            await musicChan.send(msg);
+            await testChan.send(msg);
+          }
+        } else if (now < roundEndTime.plus({ hours: roundMin })) {
+          console.log('Awaiting minimum time elapsed.');
+          console.log(roundEndTime.plus({ hours: roundMin }).toFormat('M/d/yyyy HH:mm'));
+        } else {
+          console.log('Awaiting 80%.');
+          console.log(`${(pctCheckedIn * 100).toFixed(1)}%`);
+          console.log(roundEndTime.plus({ hours: roundMin }).toFormat('M/d/yyyy HH:mm'));
+          console.log('Missing:', missingList);
+          console.log('Extra:', extraList);
         }
-      } else if (now < roundEndTime.plus({ hours: roundMin })) {
-        console.log('Awaiting minimum time elapsed.');
-        console.log(roundEndTime.plus({ hours: roundMin }).toFormat('M/d/yyyy HH:mm'));
-      } else {
-        console.log('Awaiting 80%.');
-        console.log(`${(pctCheckedIn * 100).toFixed(1)}%`);
-        console.log(roundEndTime.plus({ hours: roundMin }).toFormat('M/d/yyyy HH:mm'));
-        console.log('Missing:', missingList);
-        console.log('Extra:', extraList);
-      }
-    } else { console.log('Round in progress.'); }
-  });
+      } else { console.log('Round in progress.'); }
+    });
+  } else { console.log('Bot disabled. Round in progress?'); }
 };
 
 client.once('ready', () => {
@@ -337,6 +341,8 @@ const loadCredentials = () => {
     throw err;
   }
 };
+
+const getValue = async (rng) => getMsg(rng, await getAuthClient());
 
 const getValues = async (rng) => getMsgs(rng, await getAuthClient());
 
@@ -386,6 +392,20 @@ const getNewToken = async (oAuth2Client) => {
   });
   // This should await readline.question and
   // there's probably a similar thing to do with oAuth2Client.getToken
+};
+
+const getMsg = async (rng, auth) => {
+  const sheets = google.sheets({ version: 'v4', auth });
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: rng,
+    });
+    return response.data.values[0][0];
+  } catch (err) {
+    console.log(`getMsg API returned an error for range "${rng}"`, err);
+    throw err;
+  }
 };
 
 const getMsgs = async (rng, auth) => {
