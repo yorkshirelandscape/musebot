15879e303f6dfb6b9819b1510001820abb75bb9b
Andrew Howard <andrew.bryce.howard@gmail.com>
2021-09-02 16:11:25 -0400

    change auth scope

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
