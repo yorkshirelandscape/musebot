c352adfddcffb57855d5234d7dd7746343e86ff1
Andrew Howard <andrew.bryce.howard@gmail.com>
2022-01-01 09:54:47 -0500

    created emoji.js

--- post.js
+++ post.js
@@ -24,15 +24,6 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // time.
 const TOKEN_PATH = 'token.json';
 
-const dismoji = require('discord-emoji');
-const enm = require('emoji-name-map');
-
-const EMOJI_ONE = enm.get('one');
-// const EMOJI_TWO = enm.get('two');
-const EMOJI_BOOM = enm.get('boom');
-
-const BOT_STATE_REF = 'Dashboard!B4';
-
 const REFS = {
   round: 'Dashboard!B2',
   song: 'Dashboard!B3',
@@ -67,103 +58,7 @@ const formatOther = (text) => text.replaceAll(/(:[^:\n]+:)/g, replacement);
 
 const getMatchText = (rows) => rows.map(formatMatchRow).join('\n');
 
-const getDismojiByName = (name) => {
-  for (const cat of Object.values(dismoji)) {
-    if (typeof cat[name] !== 'undefined') {
-      return cat[name];
-    }
-  }
-  return null;
-};
 
-const getDismojiByUnicode = (uni) => {
-  for (const cat of Object.values(dismoji)) {
-    for (const emo of Object.values(cat)) {
-      if (emo === uni) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-const findEmojis = async (text) => Promise.all(Array.from(text.matchAll(/(?<=\u200b):?([^:\n]+):?(?=\u200b)/g), getEmoji));
-
-const replaceEmojis = (text, emojis) => (
-  emojis.filter((emoji) => emoji.replacement)
-    .reduce((curText, emoji) => (
-      curText.replace(emoji.text, emoji.replacement)
-    ), text)
-);
-
-const getEmoji = async (match) => {
-  const guild = client.guilds.cache.get(GUILD_ID);
-
-  const text = match[0];
-  const name = match[1];
-
-  const emoji = {
-    text,
-    name,
-    replacement: false,
-  };
-  const matchFunc = (ident) => ident.name === name;
-  try {
-    const guildEmoji = await guild.emojis.cache.find(matchFunc);
-    emoji.id = guildEmoji.id;
-    emoji.replacement = `<${text}${guildEmoji.id}>`;
-    console.log(`Custom emoji found for "${name}"`, guildEmoji.id);
-  } catch (err) {
-    console.log(`No custom emoji found for "${name}"`);
-    try {
-      const clientEmoji = await client.emojis.cache.find(matchFunc);
-      emoji.id = clientEmoji.id;
-      emoji.replacement = `<${text}${clientEmoji.id}>`;
-      console.log(`Client emoji found for "${name}"`, clientEmoji.id);
-    } catch (errClient) {
-      console.log(`No client emoji found for "${name}"`);
-    }
-    const sym = getDismojiByName(name);
-    if (sym) {
-      emoji.unicode = sym;
-      console.log(`Dismoji emoji found for "${name}"`, sym);
-    } else {
-      console.log(`No dismoji emoji found for "${name}"`);
-    }
-    const uniTest = Array.from(text.matchAll(/:([a-zA-Z0-9_]+):/g));
-    if (uniTest.length === 0) {
-      const uniMatch = getDismojiByUnicode(text);
-      if (uniMatch === true) {
-        emoji.unicode = text;
-        console.log(`Dismoji verified for "${text}"`);
-      } else {
-        console.log(`Dismoji not found for "${text}"`);
-      }
-    }
-  }
-  if (!emoji.id && !emoji.unicode) {
-    console.log(`Emoji not found for "${name}"`);
-    return {
-      text,
-      name,
-      replacement: EMOJI_BOOM,
-      unicode: EMOJI_BOOM,
-    };
-  }
-  return emoji;
-};
-
-const react = async (message, emojis) => {
-  emojis.forEach(async (emoji) => {
-    if (typeof emoji.id !== 'undefined' && emoji.id !== null) {
-      await message.react(emoji.id);
-    } else if (typeof emoji.unicode !== 'undefined') {
-      await message.react(emoji.unicode);
-    } else {
-      console.log(`Can't react with invalid emoji "${emoji.name}"`);
-    }
-  });
-};
 
 // eslint-disable-next-line no-nested-ternary, max-len
 const getMatchesCount = (round, size) => Math.min(round === 4 ? 4 : (round === 5 ? 2 : (round === 6 ? 2 : 8)), 2 ** (round - (size > 64 ? 0 : (size > 32 ? 1 : 2))));
@@ -269,34 +164,6 @@ const getNewToken = async (oAuth2Client) => {
   });
   // This should await readline.question and
   // there's probably a similar thing to do with oAuth2Client.getToken
-};
-
-const clearRngs = async (rng, auth) => {
-  const sheets = google.sheets({ version: 'v4', auth });
-  try {
-    const response = await sheets.spreadsheets.values.batchClear({
-      spreadsheetId: SPREADSHEET_ID,
-      ranges: rng,
-    });
-    return response.data;
-  } catch (err) {
-    console.log(`clearRngs API returned an error for range "${rng}"`, err);
-    throw err;
-  }
-};
-
-const getMsg = async (rng, auth) => {
-  const sheets = google.sheets({ version: 'v4', auth });
-  try {
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: SPREADSHEET_ID,
-      range: rng,
-    });
-    return response.data.values[0][0];
-  } catch (err) {
-    console.log(`getMsg API returned an error for range "${rng}"`, err);
-    throw err;
-  }
 };
 
 const getMsgs = async (rng, auth) => {
